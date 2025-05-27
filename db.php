a92e4fa9220b5f934452c5281b21241849bd9294
Mariasantia <mariashantym@gmail.com>
2025-05-27 15:09:59 +0700

    menambahkan file php

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+try {
+    $db = new PDO('sqlite:jamuku.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Koneksi gagal: " . $e->getMessage();
+}
+?>
