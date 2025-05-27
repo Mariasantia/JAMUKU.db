113d1f0fca58b3bdb521ad5b5dbde4c3120b04d9
Mariasantia <mariashantym@gmail.com>
2025-05-27 19:43:47 +0700

    Tampilan dan fungsi input produk sudah rapi

--- bayar.php
+++ bayar.php
@@ -1,45 +1,41 @@
 <?php
-include 'db.php';
 session_start();
+include 'db.php';
+
+// Simulasi proses pembayaran
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['bayar'])) {
+    // Kosongkan keranjang
+    $_SESSION['keranjang'] = [];
+    $pesan = "Pembayaran berhasil!";
+} else {
+    // Cek apakah ada barang di keranjang
+    if (empty($_SESSION['keranjang'])) {
+        $pesan = "Keranjang kosong.";
+    } else {
+        $pesan = "Total item: " . count($_SESSION['keranjang']);
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Keranjang</title>
+    <meta charset="UTF-8">
+    <title>Pembayaran</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
         <h1>Keranjang Belanja</h1>
-        <?php
-        $total = 0;
-        if (!empty($_SESSION['keranjang'])) {
-            echo "<table><tr><th>Nama</th><th>Jumlah</th><th>Subtotal</th></tr>";
-            foreach ($_SESSION['keranjang'] as $id => $jumlah) {
-                $hasil = $db->query("SELECT * FROM produk WHERE id = $id");
-                $produk = $hasil->fetchArray();
-                $subtotal = $produk['harga'] * $jumlah;
-                $total += $subtotal;
-                echo "<tr>
-                        <td>{$produk['nama']}</td>
-                        <td>$jumlah</td>
-                        <td>Rp $subtotal</td>
-                      </tr>";
-            }
-            echo "<tr><td colspan='2'><b>Total</b></td><td><b>Rp $total</b></td></tr></table>";
-        } else {
-            echo "<p>Keranjang kosong.</p>";
-        }
+        <p><?php echo $pesan; ?></p>
 
-        if (isset($_POST['bayar'])) {
-            unset($_SESSION['keranjang']);
-            echo "<p><b>Pembayaran berhasil!</b></p>";
-        }
-        ?>
-        <form method="post">
-            <button name="bayar">Bayar Sekarang</button>
-        </form>
-        <a href="index.php" class="btn">Kembali</a>
+        <?php if (!isset($_POST['bayar'])): ?>
+            <form method="post">
+                <button type="submit" name="bayar">Bayar Sekarang</button>
+            </form>
+        <?php endif; ?>
+
+        <a href="index.php">Kembali</a>
     </div>
 </body>
 </html>
